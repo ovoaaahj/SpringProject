73a8fde4ea96bd2098a7f6df52084f7418ec6308
kojeongwook <kojw1017@naver.com>
2021-01-21 19:14:22 +0900

    indexxxxx

--- spring_project/src/main/java/com/spring/controller/MainController.java
+++ spring_project/src/main/java/com/spring/controller/MainController.java
@@ -1,11 +1,6 @@
 package com.spring.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
